75b5430b75ec10043e271bf0ece338438d12acc3
Alexander Ryzhov <github@ryzhov-al.ru>
2016-01-12 09:37:59 +0300

    make-installer.sh: Add SSH keys

--- make-installer.sh
+++ make-installer.sh
@@ -11,15 +11,24 @@ mkdir $ROOT_DIR
 
 # Adding toolchain libraries
 cp -r $BUILD_DIR/toolchain/ipkg-mipselsf/libc/opt $ROOT_DIR
+cp -r $BUILD_DIR/toolchain/ipkg-mipselsf/libgcc/opt $ROOT_DIR
 
 # Adding busybox
 cp -r $BUILD_DIR/busybox-*/ipkg-install/opt $ROOT_DIR
 
+# Adding SSH keys to avoid dropbear post-install timeout
+mkdir -p $ROOT_DIR/opt/etc/dropbear
+cp $SCRIPT_DIR/dropbear_ecdsa_host_key $ROOT_DIR/opt/etc/dropbear
+cp $SCRIPT_DIR/dropbear_rsa_host_key $ROOT_DIR/opt/etc/dropbear
+
 # Adding install script
 mkdir -p $ROOT_DIR/opt/etc/init.d
 cp $SCRIPT_DIR/doinstall $ROOT_DIR/opt/etc/init.d
+chmod +x $ROOT_DIR/opt/etc/init.d/doinstall
 
 # Packing installer
 [ -f $INSTALLER ] && rm $INSTALLER
 tar -czf $INSTALLER -C $ROOT_DIR/opt bin etc lib sbin
+
+# Removing temp folder
 rm -fr $ROOT_DIR
